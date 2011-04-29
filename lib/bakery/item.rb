604e4fa55c0df16c8c0d1e8199375279197d0d69
Robin Clart <robin@charlin.lu>
2011-04-29 16:25:05 +0200

    Add documentation to all the methods in the Template module

--- lib/bakery/item.rb
+++ lib/bakery/item.rb
@@ -138,44 +138,81 @@ module Bakery
       end
     end
 
+    # == Template Lookup Procedure
+    #
+    # The template used by an item will be the first one to match an existing
+    # file in the "templates/" directory:
+    #
+    # The lookup order is as follow:
+    #
+    # - A template that match the template name supplied in the data section
+    #   of the item (1)
+    # - A template with the same basename as the item (2)
+    # - A template with the name of the item's model (3)
+    # - The index template file (4)
+    #
+    # For example a post with the following path "posts/hello-world.html.md"
+    # will use:
+    #
+    # - "templates/supplied-template-name.html.erb" (1)
+    # - "templates/hello-world.html.erb" (2)
+    # - "templates/post.html.erb" (3)
+    # - "templates/index.html.erb" (4)
+    #
+    # Note that the extension (without the ".md" for the items and without the
+    # ".erb" for a templates) should be the same to match. So if you are using
+    # ".htm" instead of ".html" in your file name your template basename should
+    # reflect this difference.
+    #
+    # Also be noted that all items starting with "index.*" won't resolve at (2)
+    # but at (4) in order to allow those items to be compiled into the
+    # template with the name of the item's model
     module Template
+      # Returns the content of the item's template.
       def template_content
         @template_content ||= File.read(template_path)
       end
 
+      # Returns the template path for the current item.
       def template_path
         @template_path ||= resolve_template_path(template_basename)
       end
 
+      # Returns the basename of the item's template (without the ".erb"
+      # extension).
       def template_basename
         available_template_names.first or fallback_template_name
       end
 
       private
 
+      # Returns an array of all the availables template names (without the
+      # ".erb" extension) for the current item except the fallback one.
       def available_template_names
         hypothetical_template_names.select do |name|
           File.exists? resolve_template_path(name)
         end
       end
 
+      # Returns an array of all the suitable template names (without the ".erb"
+      # extension) for the current item except the fallback one.
       def hypothetical_template_names
         [data.template, base_template_name, model_template_name].compact
       end
 
-      def base_template_name
+      def base_template_name #:nodoc:
         basename unless basename == fallback_template_name
       end
 
-      def model_template_name
+      def model_template_name #:nodoc:
         [modelname, extname].join
       end
 
-      def fallback_template_name
+      def fallback_template_name #:nodoc:
         "index.html"
       end
 
-      def resolve_template_path(name)
+      def resolve_template_path(name) #:nodoc:
         File.join("templates", "#{name}.erb")
       end
     end
