10653aaba7e22585c8aba562f7b0c87cdaf7ab11
Robin Clart <robin@charlin.lu>
2011-06-25 16:45:26 +0200

    Fix the desc and namespace for the template gen

--- lib/bakery/generators/template.rb
+++ lib/bakery/generators/template.rb
@@ -1,10 +1,9 @@
 module Bakery
   module Generators
     class Template < Base
-      namespace "generate page"
-      desc "Create a new page."
+      namespace "generate template"
+      desc "Create a new template."
       argument :title
-      class_option :directory, optional: true, default: "", aliases: ["-d"]
       class_option :extension, optional: true, default: "html", aliases: ["-e"]
       class_option :force, optional: true, default: false, aliases: ["-f"]
 
