7cba392018fe06b1b4269996ba3d4039d7486b59
Robin Clart <robin@charlin.lu>
2011-06-25 04:47:22 +0200

    Oups again. Forgat that the Generate constant is under Commands and not Interface

--- lib/bakery/interface.rb
+++ lib/bakery/interface.rb
@@ -3,15 +3,15 @@ module Bakery
     def start
       # Invoke the output command if there is no arguments or if the first
       # argument is an existing file or the force flag.
-      ARGV.unshift("output") if Generate.implicit_output?
+      ARGV.unshift("output") if Commands::Generate.implicit_output?
 
       # Invoke with generate without explicitly asking for it.
-      ARGV.unshift("generate") if Generate.implicit_generator?
+      ARGV.unshift("generate") if Commands::Generate.implicit_generator?
 
       command_name = ARGV.shift
 
       begin
-        command = command_path(command_name).camelize.constantize
+        command = Commands.command_path(command_name).camelize.constantize
       rescue NameError
         puts "Invalid command (#{command_name})."
         exit
