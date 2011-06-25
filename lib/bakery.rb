a3b60568f5bdf582b0185a1575d730351f3975ba
Robin Clart <robin@charlin.lu>
2011-06-25 06:55:01 +0200

    Double quote for all plus active_support autoload

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -1,25 +1,26 @@
-require 'pathname'
-require 'date'
-require 'time'
-require 'yaml'
-require 'ostruct'
-require 'erb'
-require 'redcarpet'
-require 'active_support/inflector'
+require "pathname"
+require "date"
+require "time"
+require "yaml"
+require "ostruct"
+require "erb"
+require "redcarpet"
+require "active_support/dependencies/autoload"
+require "active_support/inflector"
+
+require "bakery/version"
 
 module Bakery
-  autoload :VERSION,    "bakery/version"
+  extend ActiveSupport::Autoload
 
-  autoload :Routing,    "bakery/routing"
-
-  autoload :Page,       "bakery/page"
-  autoload :Output,     "bakery/output"
-  autoload :Template,   "bakery/template"
-  autoload :Partial,    "bakery/partial"
-  autoload :Context,    "bakery/context"
-  autoload :Helpers,    "bakery/helpers"
-
-  autoload :Interface,  "bakery/interface"
-  autoload :Commands,   "bakery/commands"
-  autoload :Generators, "bakery/generators"
+  autoload :Routing
+  autoload :Page
+  autoload :Output
+  autoload :Template
+  autoload :Partial
+  autoload :Context
+  autoload :Helpers
+  autoload :Interface
+  autoload :Commands
+  autoload :Generators
 end
