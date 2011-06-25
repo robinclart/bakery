917c9be64434e67120688ee48eab4cc2ab4a8425
Robin Clart <robin@charlin.lu>
2011-06-25 04:40:09 +0200

    Move the CLI into its own class: Interface. Tadaaa

--- lib/bakery/commands.rb
+++ lib/bakery/commands.rb
@@ -1,25 +1,5 @@
 module Bakery
   module Commands extend self
-    def run
-      # Invoke the output command if there is no arguments or if the first
-      # argument is an existing file or the force flag.
-      ARGV.unshift("output") if Generate.implicit_output?
-
-      # Invoke with generate without explicitly asking for it.
-      ARGV.unshift("generate") if Generate.implicit_generator?
-
-      command_name = ARGV.shift
-
-      begin
-        klass = command_path(command_name).camelize.constantize
-      rescue NameError
-        puts "Invalid command (#{command_name})."
-        exit
-      end
-
-      klass.start
-    end
-
     def alias_command(scope, *aliases)
       @@_commands ||= {}
       @@_commands[scope.to_s] ||= []
---
+++ lib/bakery/interface.rb
@@ -0,0 +1,23 @@
+module Bakery
+  module Interface
+    def start
+      # Invoke the output command if there is no arguments or if the first
+      # argument is an existing file or the force flag.
+      ARGV.unshift("output") if Generate.implicit_output?
+
+      # Invoke with generate without explicitly asking for it.
+      ARGV.unshift("generate") if Generate.implicit_generator?
+
+      command_name = ARGV.shift
+
+      begin
+        command = command_path(command_name).camelize.constantize
+      rescue NameError
+        puts "Invalid command (#{command_name})."
+        exit
+      end
+
+      command.start
+    end
+  end
+end
