44676ecd531d14ebac1e348e9f54cd1adc961345
Robin Clart <robin@charlin.lu>
2011-06-25 04:41:55 +0200

    Oups forgot to autoload the interface

--- lib/bakery.rb
+++ lib/bakery.rb
@@ -20,6 +20,8 @@ module Bakery
   autoload :Partial,    "bakery/partial"
   autoload :Context,    "bakery/context"
   autoload :Helpers,    "bakery/helpers"
+
+  autoload :Interface,  "bakery/interface"
   autoload :Commands,   "bakery/commands"
   autoload :Generators, "bakery/generators"
 
